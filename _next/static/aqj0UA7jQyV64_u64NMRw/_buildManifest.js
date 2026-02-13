6a0e7137e48ba86da7cef701dc365047d3c26656
ketarou85 <ketarou85@gmail.com>
2026-02-13 20:52:34 +0900

    Updates

---
+++ _next/static/aqj0UA7jQyV64_u64NMRw/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/vivehong//_next/:path+",
+        "destination": "/vivehong/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
